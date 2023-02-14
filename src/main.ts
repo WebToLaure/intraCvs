81e7a956e9edb4718691b480fd74eef92c43a6fe
WebToLaure <lmarmus@hotmail.fr>
2023-02-14 23:16:25 +0100

    feat : swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule,DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
-async function bootstrap() {
+async function bootstrap() {  //fonction qui démarre notre appli
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
-}
-bootstrap();
+  const config = new DocumentBuilder()
+  .setTitle('Intra-CVS API')
+  .setDescription('API CREATION CV')
+  .setVersion('1.0')
+  .build();
+const document = SwaggerModule.createDocument(app, config);
+SwaggerModule.setup('api', app, document);
+
+
+app.useGlobalPipes(new ValidationPipe({
+
+    whitelist: true,
+    forbidNonWhitelisted: true,
+
+  })
+)};

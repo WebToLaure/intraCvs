f0d3279b7ae521969020ef6653ba4e39dd7abb1c
WebToLaure <lmarmus@hotmail.fr>
2023-02-17 09:20:05 +0100

    feat: module formation

--- src/formations/formations.controller.ts
+++ src/formations/formations.controller.ts
@@ -1,36 +1,87 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, HttpStatus,ParseIntPipe, HttpException, Request, UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import { ApiBody, ApiTags, ApiOperation } from '@nestjs/swagger';
 import { FormationsService } from './formations.service';
 import { CreateFormationDto } from './dto/create-formation.dto';
 import { UpdateFormationDto } from './dto/update-formation.dto';
 import { UsersService } from 'src/users/users.service';
 
+@ApiTags("FORMATIONS")
 @Controller('formations')
 export class FormationsController {
   constructor(private readonly formationsService: FormationsService,
     private readonly usersService: UsersService) { }
 
+  @ApiBody({ type: CreateFormationDto })
+  @UseGuards(JwtAuthGuard)
   @Post()
-  create(@Body() createFormationDto: CreateFormationDto) {
-    return this.formationsService.create(createFormationDto);
+  @ApiOperation({ summary: "Création d'une formation sur compte utilisateur" })
+  async createFormation(@Body() createFormationDto: CreateFormationDto, @Request() req) {
+
+    if (await this.formationsService.findByFormationAndUser(req.user.userId, createFormationDto.formation)) {
+      throw new HttpException("Formation déjà renseignée", HttpStatus.NOT_ACCEPTABLE);
+    }
+    const user = await this.usersService.findUserById(req.user.userId) 
+    return await this.formationsService.createFormation(createFormationDto, user);
   }
 
+
+  @ApiBody({ type:CreateFormationDto })
+  @UseGuards(JwtAuthGuard)
   @Get()
-  findAll() {
-    return this.formationsService.findAll();
+  @ApiOperation({ summary: "Récupération de l'ensemble des formations utilisateurs" })
+  async findAllForm() {
+    return await this.formationsService.findAllFormations();
   }
 
+
+  @UseGuards(JwtAuthGuard)
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.formationsService.findOne(+id);
+  @ApiOperation({ summary: "Récupération d'une formation utilisateur par son id" })
+  async findFormationById(@Param('id', ParseIntPipe) id: number) {
+    const formation = await this.formationsService.findFormationById(id);
+    if (!formation) {
+      throw new HttpException("cette formation n'existe pas", HttpStatus.BAD_REQUEST);
+    }
+    return formation;
   }
+
+
+  @UseGuards(JwtAuthGuard)
+  @Get('formations/:name')
+  async findFormationByName(@Param('name') name: string, @Request() req) {
+    const formation = await this.formationsService.findFormationByName(name);
+    if (!formation) {
+      throw new HttpException("Aucune formation trouvée", HttpStatus.NOT_FOUND);
+    }
+    return await this.formationsService.findByFormationAndUser(req.user.userId, req.user.formation);
+  }
+
 
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateFormationDto: UpdateFormationDto) {
     return this.formationsService.update(+id, updateFormationDto);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.formationsService.remove(+id);
+  @ApiOperation({ summary: "Suppression d'une formation" })
+  async deleteFormation(@Param('id', ParseIntPipe) id: number, @Request() req) {
+    const formation= await this.formationsService.findFormationById(id);
+
+    if (!formation) {
+
+      throw new HttpException("Formation introuvable.", HttpStatus.NOT_FOUND);
+    }
+
+    if (req.user.userId !== formation.user.id) {
+
+      throw new HttpException(" Non autorisé.", HttpStatus.FORBIDDEN);
+    }
+    if (await this.formationsService.deleteFormation(id)) {
+
+      throw new HttpException("Formation supprimée.", HttpStatus.OK);
+    }
+    throw new HttpException("Suppression impossible.", HttpStatus.BAD_REQUEST);
   }
 }
--- src/formations/formations.service.ts
+++ src/formations/formations.service.ts
@@ -1,27 +1,51 @@
 import { Injectable } from '@nestjs/common';
 import { CreateFormationDto } from './dto/create-formation.dto';
 import { UpdateFormationDto } from './dto/update-formation.dto';
+import { Like } from 'typeorm';
 import { User } from 'src/users/entities/user.entity';
+import { Formation } from './entities/formation.entity';
 
 @Injectable()
 export class FormationsService {
-  create(createFormationDto: CreateFormationDto) {
-    return 'This action adds a new formation';
+  async createFormation(createFormationDto: CreateFormationDto, user: User) {
+    const formations = Formation.create({ ...createFormationDto });
+    delete user.password
+    formations.user = user;
+    return await formations.save()
   }
 
-  findAll() {
-    return `This action returns all formations`;
+  async findAllFormations(): Promise<Formation[]> {
+    return await Formation.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} formation`;
+  async findFormationById(id: number) {
+    const formation = await Formation.findOne({ relations: { user: true }, where: { id } })
+    if (!formation) {
+      return undefined;
+    }
+    return formation;
   }
 
   update(id: number, updateFormationDto: UpdateFormationDto) {
     return `This action updates a #${id} formation`;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} formation`;
+  async deleteFormation(id: number) {
+    return (await Formation.delete({ id })).affected;;
   }
+
+
+  async findFormationByName(formation: string) {
+    const name = await Formation.findBy({ formation: Like(`%${formation}%`) });
+    if (name.length === 0) {
+      return undefined
+    }
+    return name;
+  }
+
+
+  async findByFormationAndUser(userId: number, formation: string) {
+    return await Formation.findOne({ where: { user: { id: userId }, formation: formation } });
+  }
+
 }

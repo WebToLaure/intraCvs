0f51f251369ff35eda13ae234f091ac10a5a41ab
sandrine-proenca <sandrine.proenca@sncf.fr>
2023-02-21 15:34:37 +0100

    fix create competence

--- src/experiences/experiences.controller.ts
+++ src/experiences/experiences.controller.ts
@@ -1,8 +1,9 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Request, HttpException, HttpStatus } from '@nestjs/common';
 import { ExperiencesService } from './experiences.service';
 import { CreateExperienceDto } from './dto/create-experience.dto';
 import { UpdateExperienceDto } from './dto/update-experience.dto';
 import { UsersService } from 'src/users/users.service';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 
 
 
@@ -10,11 +11,32 @@ import { UsersService } from 'src/users/users.service';
 export class ExperiencesController {
   constructor(private readonly experiencesService: ExperiencesService,
     private readonly usersService: UsersService) { }
-
+  // Création d'une expérience
+  @UseGuards(JwtAuthGuard)
   @Post()
-  create(@Body() createExperienceDto: CreateExperienceDto) {
-    return this.experiencesService.create(createExperienceDto);
+  async create(@Body() createExperienceDto: CreateExperienceDto, @Request() req) {
+    // Vérifier si l'expérience existe déjà pour le user
+    const experienceExist = await this.experiencesService.findByExperienceAndUser(req.user.userId, createExperienceDto.intitulé_poste)
+    if(experienceExist){
+      throw new HttpException('L\'expérience existe déjà', HttpStatus.NOT_ACCEPTABLE)
+    }
+    // Créer l'expérience pour le user défini
+    const user =await this.usersService.findOne(req.user.userId)
+    const newExperience = await this.experiencesService.create(createExperienceDto, user)
+    return {
+      statusCode: 201,
+      message: `Création d'une nouvelle expérience`,
+      data: newExperience
+    }
   }
+
+  //
+
+
+
+
+
+
 
   @Get()
   findAll() {
